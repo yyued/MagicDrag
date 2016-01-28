843d2d1dd087bf5e3a019253b3b688d7b3ee9af4
PonyCui <cuiminghui@yy.com>
2016-01-28 16:36:47 +0800

    Add delay support.

--- MagicDrag/Animator/MGDAnimator.swift
+++ MagicDrag/Animator/MGDAnimator.swift
@@ -30,98 +30,125 @@ protocol MGDAnimationMove {
     
 }
 
+protocol MGDAnimationZoom {
+    
+    var zoomIn: CGPoint { get set }
+    var zoomOut: CGPoint { get set }
+    
+}
+
 class MGDAnimator {
     
-    static func delayProgress(item: UIView, viewProgress: CGFloat) -> CGFloat? {
-        if let item = item as? MGDAnimatable {
-            if item.delaysOfAnimation > 0.0 && item.delaysOfAnimation < 1.0 {
-                if viewProgress < 0.0 && 1.0 - fabs(viewProgress) < item.delaysOfAnimation {
-                    return nil
-                }
-                else if viewProgress > 0.0 && viewProgress < item.delaysOfAnimation {
-                    return nil
-                }
-                else {
-                    return item.delaysOfAnimation
-                }
-            }
-        }
-        return 0.0
-    }
-    
     static func fadeIn(item: UIView, viewProgress: CGFloat) {
-        if viewProgress <= -1.0 {
-            item.alpha = 0.0
-        }
-        else if viewProgress <= 0.0 {
-            guard let delayProgress = self.delayProgress(item, viewProgress: viewProgress) else {
-                item.alpha = 0.0
-                return
-            }
-            item.alpha = 1.0 + viewProgress * 1.0 / (1.0 - delayProgress)
+        let calculatedProgress = self.delayProgress(item, viewProgress: viewProgress) ?? viewProgress
+        if let alpha = self.inValue(0.0, toValue: 1.0, viewProgress: calculatedProgress) {
+            item.alpha = alpha
         }
     }
     
     static func fadeOut(item: UIView, viewProgress: CGFloat) {
-        if viewProgress >= 1.0 {
-            item.alpha = 0.0
-        }
-        else if viewProgress >= 0.0 {
-            item.alpha = 1.0 - viewProgress
+        let calculatedProgress = self.delayProgress(item, viewProgress: viewProgress) ?? viewProgress
+        if let alpha = self.outValue(1.0, toValue: 0.0, viewProgress: calculatedProgress) {
+            item.alpha = alpha
         }
     }
     
     static func moveIn(item: UIView, from: CGPoint, viewProgress: CGFloat) {
-        if viewProgress <= -1.0 {
-            if from.x != 0.0, let xConstraint = item.xConstraint {
-                xConstraint.constant = xConstraint.MGD_originConstant! + from.x
-            }
-            if from.y != 0.0, let yConstraint = item.yConstraint {
-                yConstraint.constant = yConstraint.MGD_originConstant! + from.y
+        let calculatedProgress = self.delayProgress(item, viewProgress: viewProgress) ?? viewProgress
+        if let constraint = item.xConstraint, let origin = constraint.MGD_originConstant {
+            if let value = self.inValue(origin + from.x, toValue: origin, viewProgress: calculatedProgress) {
+                constraint.constant = value
             }
         }
-        else if viewProgress < 0.0 {
-            if from.x != 0.0, let xConstraint = item.xConstraint {
-                xConstraint.constant = xConstraint.MGD_originConstant! - viewProgress * from.x
-            }
-            if from.y != 0.0, let yConstraint = item.yConstraint {
-                yConstraint.constant = yConstraint.MGD_originConstant! - viewProgress * from.y
-            }
-        }
-        else if viewProgress == 0.0 {
-            if from.x != 0.0, let xConstraint = item.xConstraint {
-                xConstraint.constant = xConstraint.MGD_originConstant!
-            }
-            if from.y != 0.0, let yConstraint = item.yConstraint {
-                yConstraint.constant = yConstraint.MGD_originConstant!
+        if let constraint = item.yConstraint, let origin = constraint.MGD_originConstant {
+            if let value = self.inValue(origin + from.y, toValue: origin, viewProgress: calculatedProgress) {
+                constraint.constant = value
             }
         }
     }
     
     static func moveOut(item: UIView, to: CGPoint, viewProgress: CGFloat) {
+        let calculatedProgress = self.delayProgress(item, viewProgress: viewProgress) ?? viewProgress
+        if let constraint = item.xConstraint, let origin = constraint.MGD_originConstant {
+            if let value = self.outValue(origin, toValue: origin + to.x, viewProgress: calculatedProgress) {
+                constraint.constant = value
+            }
+        }
+        if let constraint = item.yConstraint, let origin = constraint.MGD_originConstant {
+            if let value = self.outValue(origin, toValue: origin + to.y, viewProgress: calculatedProgress) {
+                constraint.constant = value
+            }
+        }
+    }
+    
+    static func zoomIn(item: UIView, from: CGPoint, viewProgress: CGFloat) {
+        let calculatedProgress = self.delayProgress(item, viewProgress: viewProgress) ?? viewProgress
+        if let x = self.inValue(from.x, toValue: 1.0, viewProgress: calculatedProgress),
+           let y = self.inValue(from.y, toValue: 1.0, viewProgress: calculatedProgress) {
+            item.transform = CGAffineTransformMakeScale(x, y)
+        }
+    }
+    
+    static func zoomOut(item: UIView, to: CGPoint, viewProgress: CGFloat) {
+        let calculatedProgress = self.delayProgress(item, viewProgress: viewProgress) ?? viewProgress
+        if let x = self.outValue(1.0, toValue: to.x, viewProgress: calculatedProgress),
+            let y = self.outValue(1.0, toValue: to.y, viewProgress: calculatedProgress) {
+                item.transform = CGAffineTransformMakeScale(x, y)
+        }
+    }
+    
+}
+
+extension MGDAnimator {
+    
+    static func delayProgress(item: UIView, viewProgress: CGFloat) -> CGFloat? {
+        if let item = item as? MGDAnimatable {
+            if item.delaysOfAnimation > 0.0 {
+                let absViewProgress = viewProgress < 0.0 ? 1.0 + viewProgress : viewProgress
+                if item.delaysOfAnimation > absViewProgress {
+                    if viewProgress < 0.0 {
+                        return -1.0
+                    }
+                    else {
+                        return 0.0
+                    }
+                }
+                else {
+                    let absDelayProgress = (1.0 / (1.0 - item.delaysOfAnimation)) * absViewProgress + (-item.delaysOfAnimation / (1.0 - item.delaysOfAnimation))
+                    if viewProgress < 0.0 {
+                        return absDelayProgress - 1.0
+                    }
+                    else {
+                        return absDelayProgress
+                    }
+                }
+            }
+        }
+        return nil
+    }
+    
+    static func inValue(fromValue: CGFloat, toValue: CGFloat, viewProgress: CGFloat) -> CGFloat? {
+        if viewProgress <= -1.0 {
+            return fromValue
+        }
+        else if viewProgress <= 0.0 {
+            let viewProgress = viewProgress + 1.0
+            return fromValue + (toValue - fromValue) * viewProgress
+        }
+        else {
+            return nil
+        }
+    }
+    
+    static func outValue(fromValue: CGFloat, toValue: CGFloat, viewProgress: CGFloat) -> CGFloat? {
         if viewProgress >= 1.0 {
-            if to.x != 0.0, let xConstraint = item.xConstraint {
-                xConstraint.constant = xConstraint.MGD_originConstant! + to.x
-            }
-            if to.y != 0.0, let yConstraint = item.yConstraint {
-                yConstraint.constant = yConstraint.MGD_originConstant! + to.y
-            }
+            return toValue
         }
-        else if viewProgress > 0.0 {
-            if to.x != 0.0, let xConstraint = item.xConstraint {
-                xConstraint.constant = xConstraint.MGD_originConstant! + viewProgress * to.x
-            }
-            if to.y != 0.0, let yConstraint = item.yConstraint {
-                yConstraint.constant = yConstraint.MGD_originConstant! + viewProgress * to.y
-            }
+        else if viewProgress >= 0.0 {
+            return fromValue + (toValue - fromValue) * viewProgress
         }
-        else if viewProgress == 0.0 {
-            if to.x != 0.0, let xConstraint = item.xConstraint {
-                xConstraint.constant = xConstraint.MGD_originConstant!
-            }
-            if to.y != 0.0, let yConstraint = item.yConstraint {
-                yConstraint.constant = yConstraint.MGD_originConstant!
-            }
+        else {
+            return nil
         }
     }
     
